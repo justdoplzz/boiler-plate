9626c4f640c259566734f49146c8401170ae2d49
khs <justdoplzz@gmail.com>
2024-02-01 17:45:59 +0900

    [server/client 분리] react 추가

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from 'react-router-dom';
+
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route exact path="/" element={LandingPage()}/>
+          <Route exact path="/login" element={LoginPage()}/>
+          <Route exact path="/register" element={RegisterPage()}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
