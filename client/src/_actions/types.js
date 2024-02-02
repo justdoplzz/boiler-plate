39d900e3f9a2083ce6427b6c47970d258ef67926
khs <justdoplzz@gmail.com>
2024-02-02 14:29:28 +0900

    [api] 로그인 페이지 생성, 기능 구현 + redux

---
+++ client/src/_actions/types.js
@@ -0,0 +1,3 @@
+// type 관리하는 곳
+
+export const LOGIN_USER = 'login_user';
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from 'react-redux';
+import { loginUser } from "../../../_actions/user_action";
 
-function LoginPage(){
+function LoginPage(props){
+    const dispatch = useDispatch();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onEmailHandler = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const onPasswordHandler = e => {
+        setPassword(e.target.value);
+    }
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        const body = {
+            email: email,
+            password, password,
+        }
+
+        dispatch(loginUser(body))
+            .then(response => {
+                if(response.payload.loginSuccess){
+                    props.history.push('/')
+                } else{
+                    alert('Error');
+                }
+            })
+    }
+
+
     return(
-        <div>
-            LoginPage
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
+            <form style={{ display: 'flex', flexDirection: 'column' }}
+                    onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={email} onChange={onEmailHandler}/>
+                <label>Password</label>
+                <input type="password" value={password} onChange={onPasswordHandler}/>
+                <br />
+                <button>
+                    Login
+                </button>
+            </form>
         </div>
     )
 }
