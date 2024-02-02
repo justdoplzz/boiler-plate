5268209e294284064e7077dd71b74aa74721622d
khs <justdoplzz@gmail.com>
2024-02-02 13:30:14 +0900

    [redux] redux 기초

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,24 @@ import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import 'antd/dist/antd.css';
+
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import { thunk } from 'redux-thunk';
+import Reducer from './_reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, thunk)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStoreWithMiddleware(Reducer,
+                      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+                      window.__REDUX_DEVTOOLS_EXTENSION__()
+                    )}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
